98b782eea56b942599f785f25fcbcaba172c3346
Jimmy Hillis <jimmy@hillis.me>
2013-03-20 11:15:40 +0800

    Upgrading to version 0.2 with grunt control

--- readme.md
+++ readme.md
@@ -1,18 +1,27 @@
-# Le Carte
+# Hatchling Map: Le Carte
 ### A *hatchdling* jQuery plugin for Google Map functionality
 
-The Mapping functionality provided sets up some useful boilerplate and a
-jQuery plugin for quickly building maps directly from markup. It also has a
-simple JS API for adding pins and popups that allow for easily styled (CSS)
-elements on your map.
+The core functionality of this plugin provides useful Google Map wrappers with
+a jQuery plugin for quickly building maps directly from data provided in HTML
+markup.
 
-## Usage
+There is also a simple API for adding pins and popups that allow front end
+developers to easily style many elements directly through CSS.
 
-### Requirements
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/hatchddigital/jquery.lecarte/master/dist/hatchling.map.min.js
+[max]: https://raw.github.com/hatchddigital/jquery.lecarte/master/dist/hatchling.map.js
+
+Include file within your project through HTML (or preferably a single JS file)
+with minification.
+
+### JavaScript Requirements
 [jQuery 1.7+](jquery.com)
 [Google Maps API v3](https://developers.google.com/maps/documentation/javascript/reference)
 
-### HTML Markup
+### Required markup
 The only required markup is the element where the map will be placed. Be aware
 this element REQUIRES a height and width set for Google Maps to function
 correctly.
@@ -20,19 +29,27 @@ correctly.
 You may also apply the address or geo co-ordinates directly to the element as
 data- elements to provid a non-JS required implementation of Le Carte.
 
-*Note*: I'm using HAML markup for simplicity, and because it's useful to know.
+```html
+<script src="jquery.js"></script>
+<script src="dist/hatchling.map.min.js"></script>
+...
+<div class="lecarte" data-address="100 Hatchd Street, Perth"></div>
+```
 
-    .lecarte[data-address="100 Hatchd Street, Perth"]
-
-### JavaScript attach
-Simple jQuery plugin attach for any tabbed elements you've built. You can
-pass any Google Map options that you would like to have them applied during
-initialization. See [Google Maps APIv3](https://developers.google.com/maps/documentation/javascript/reference#MapOptions)
-for more information.
-
-This plugin, using Google Maps requires the Google Maps APIv3 and should be
-included before you include the `lecarte.js` script.
+```javascript
+<script>
+jQuery(function($) {
+  $('.lecarte').lecarte();
+});
+</script>
+```
 
 ## Credits
 Hatchd Digital <http://hatchd.com.au>
 Jimmy Hillis <jimmy@hatchd.com.au>
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
--- src/hatchling.map.js
+++ src/hatchling.map.js
@@ -1,23 +1,10 @@
 /**
- * HATCHD DIGITAL EMPTY COOP WEB APPLICATION FRAMEWORK
- *
- * ATTRIBUTION-NONCOMMERCIAL-SHAREALIKE 3.0 UNPORTED
- *
- * THE WORK (AS DEFINED BELOW) IS PROVIDED UNDER THE TERMS OF THIS CREATIVE
- * COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED BY
- * COPYRIGHT AND/OR OTHER APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS
- * AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
- *
- * BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE
- * TO BE BOUND BY THE TERMS OF THIS LICENSE. TO THE EXTENT THIS LICENSE MAY
- * BE CONSIDERED TO BE A CONTRACT, THE LICENSOR GRANTS YOU THE RIGHTS
- * CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND
- * CONDITIONS.
+ * HATCHD DIGITAL MAP HATCHLING for Google Maps API v3
  *
  * This code has been developed in house at HATCHD DIGITAL.
  * @see http://hatchd.com.au
  *
- * DEVELOPER USAGE:
+ * FOR DEVELOPERS:
  *
  * ALL external libraries and should be imported here, using a buildout
  * application e.g. CodeKit. This vesion of the file should be pretty,
@@ -25,24 +12,32 @@
  * Your site should always use /app-min.js when loading, which contains
  * a minified version of this script prepended with all external scripts.
  *
- * REQUIRED
+ * REQUIRED FRAMEWORKS
+ *
  * @required jquery (v1.7.0+)
  *
- * IMPORTS
- * @import hatchdlings.module.js
+ * CODE IMPORTS
+ *
+ * @codekit-prepend "google.maps.infobox.js";
  *
  * VALIDATION
+ *
  * All code must validate with JSHint (http://www.jshint.com/) to be launched
  * within a LIVE web application. NO debug code should remain in your final
  * versions e.g. remove EVERY reference to window.console.log().
  *
  * STYLE
+ *
  * All code should be within 79 characters WIDE to meet standard Hatchd
  * protocol. Reformat code cleanly to fit within this tool.
  *
- * jshint = { "laxcomma": true, "laxbreak": true, "browser": true }
+    jshint = {
+        "laxcomma": true,
+        "laxbreak": true,
+        "browser": true
+    }
  *
- * HATCHDLING LE CARTE MODULE
+ * HATCHDLING MAP MODULE
  *
  * This set of code allows us to manage Mapping functionality using Google
  * Maps to add/remove/hide and play with icons and boxes in an easily-styled
@@ -53,37 +48,33 @@
  *
  */
 
-;(function() {
+(function($) {
+    "use strict";
+    /*global google:true, InfoBox:true*/
 
     /**
-     * Attaches our Facilities Google Map to an element in the DOM
+     * Attaches our  Google Map to an element in the DOM
      * and puts the default markers into the system ready for dealing
      * with moving around the page
      */
-    var Carte = function (element, options) {
+    var Map = function (element, options) {
 
-        // Check provided options and set defaults where required
-        options = options || {};
-        element = element || options.element;
-
-        // Set default map position to Perth, Western Australia!
-        options.latitude = options.latitude || -31.95391;
-        options.longitude = options.longitude || 115.858512;
+        // Preset default options
+        options = $.extend({
+            latitude: -31.95391,
+            longitude:  115.858512
+        }, options);
 
         // An element MUST be provided to attach the Google Map into the DOM
         if (!element || !$(element).length) {
             return false;
         }
 
-        // Required data
-        var that = this
-
-        // element that's used to attach the map into the DOM
-          , $element = $(element) || false
+        var
 
         // all available markers (facilities) for messing with
         // default options for the google map (see Maps API v3.)
-          , google_map_options = $.extend({
+            google_map_options = $.extend({
                 zoom: 11,
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 center: new google.maps.LatLng(
@@ -97,21 +88,22 @@
         this.available_markers = [];
 
         // Single info window
-        this.infowindow = new google.maps.InfoWindow({
-            'pixelOffset': new google.maps.Size(30, -30)
+        this.infowindow = new InfoBox({
+            alignBottom: true,
+            pixelOffset: new google.maps.Size(60, 100),
+            closeBoxURL: 'images/map-close.png',
+            closeBoxMargin: '10px',
+            infoBoxClearance: new google.maps.Size(40, 40)
         });
-
         return this;
-
     };
 
     /**
      * Puts a pin on the map with the provided details. Returns the
      * Google Map object for chaining.
      */
-    Carte.prototype.addMarker = function (location, title, id, marker_icon,
+    Map.prototype.addMarker = function (location, title, id, marker_icon,
                                           description, type) {
-
         var marker = new google.maps.Marker({
                 position: location,
                 map: this.google_map,
@@ -119,42 +111,43 @@
                 icon: marker_icon,
                 optimized: false
             });
-
         marker.id = id;
         marker.infowindow = this.infowindow;
         marker.description = description;
         marker.type = type || {};
         this.available_markers.push(marker);
-
         // Setup HTML popup for Marker
         google.maps.event.addListener(marker, 'click', function() {
             marker.map.panTo(marker.position);
             marker.infowindow.setContent(marker.description);
             marker.infowindow.open(marker.map, marker);
         });
-
     };
 
     /**
      * Center the map on specified marker. Usually will be attached to a JS
      * user event (like a click) passing the ID or title
      */
-    Carte.prototype.centerOnMarker = function (settings) {
-        var search_id = parseInt(settings.id, 10) || false
+    Map.prototype.centerOnMarker = function (settings) {
+        var search_id = settings.id || false
           , search_title = settings.title || false
+          , zoom = settings.zoom || false
           , that = this;
         // Required settings not provided, return false and do nothing
         if (!search_id && !search_title) {
             return false;
         }
         // Search for the provided marker by title and ID and center
-        $.each(this.available_markers, function(index, this_marker) {
-            if (this_marker.title === search_title ||
-                this_marker.id === search_id) {
-                that.google_map.panTo(this_marker.position);
-                if (that.infowindow.getContent() !== this_marker.description) {
-                    that.infowindow.setContent(this_marker.description);
-                    that.infowindow.open(this_marker.map, this_marker);
+        $.each(this.available_markers, function(index, marker) {
+            if (marker.title === search_title ||
+                    marker.id === search_id) {
+                that.google_map.panTo(marker.position);
+                if (that.infowindow.getContent() !== marker.description) {
+                    that.infowindow.setContent(marker.description);
+                    that.infowindow.open(marker.map, marker);
+                    if (zoom) {
+                        that.google_map.setZoom(zoom);
+                    }
                 }
             }
         });
@@ -164,42 +157,42 @@
      * Toggle visibility of a specific type (or all) markers
      * on currently on the map.
      */
-    Carte.prototype.toggleMarkers = function (settings) {
+    Map.prototype.toggleMarkers = function (settings) {
         var type = settings.type || false
           , action = settings.action || false;
-        $.each(this.available_markers, function(index, this_marker) {
-            if (!type || this_marker.type === type) {
-                this_marker.setVisible(action);
-                if (!action && this.infowindow.getContent() === this_marker.description) {
-                    this_marker.infowindow.close();
+        $.each(this.available_markers, function(index, marker) {
+            if (!type || marker.type === type) {
+                marker.setVisible(action);
+                if (!action &&
+                    this.infowindow.getContent() === marker.description) {
+                    marker.infowindow.close();
                 }
             }
         });
     };
 
     /**
-     * jQuery plugin attaches map to a specific DOM elements
+     * jQuery plugin to build Hatchling Maps on provided element blocks.
+     *
+     * > USAGE:
+     * > var map = $('#element_id').map({ ... });
+     * > map.addMarker({ ... });
+     *
+     * @param {object} options Key/pair options to be use on a Google Map
+     *                         as described http://goo.gl/PwWXs
      */
-    $.fn.lecarte = function(options) {
-
+    $.fn.map = function(options) {
         options = options || {};
-
         this.each(function() {
-
             var $this = $(this)
-              , address = ($this.attr('data-address') !== undefined) ? $this.attr('data-address') : false
               , geocoder = new google.maps.Geocoder()
-              , map_options = {
-                    zoom: 11,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                }
-              , map = $this.data('carte');
-
-            // If we can't find an existing modal, create a new one
+              , map = $this.data('map')
+              , address = (($this.attr('data-address') !== undefined) ?
+                              $this.attr('data-address') : false);
+            // If we can't find an existing map, create and store
             if (!map) {
-                $this.data('carte', (map = new Carte(this, options)));
+                $this.data('map', (map = new Map(this, options)));
             }
-
             // Geocode the address, and create the map
             if (address) {
                 geocoder.geocode( {'address': address }, function (results, status) {
@@ -213,16 +206,11 @@
                     }
                 });
             }
-
-            // Add a loop link back to the element in the DOM
-            map.$element = $this;
-
         });
-
+        // Single map attach return the created object
         if (this.length === 1) {
-            return $(this).data('carte');
+            return $(this).data('map');
         }
-
     };
 
 }(window.jQuery));
